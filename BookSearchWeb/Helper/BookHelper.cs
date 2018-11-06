2e6f162910b440e544a8fbdd7e8e0ba50018d9af
Andreas Kisch <Andreas@kisch.se>
2018-11-06 14:36:31 +0100

    Added start web to use API

---
+++ BookSearchWeb/Helper/BookHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BookSearchWeb
+{
+    public class BookHelper
+    {
+        public HttpClient BookAPI { get; set; }
+
+        public BookHelper ()
+        {
+           BookAPI = new HttpClient();
+           BookAPI.BaseAddress = new Uri("http://localhost:53658");
+           
+
+        }
+    }
+}
